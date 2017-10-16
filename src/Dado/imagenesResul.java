3fc379378da6a8382adc6e625c9a777280bcb051
carlosD19 <carlos.d.m.s@hotmail.com>
2017-10-16 16:04:20 -0600

    Dado

--- src/Dado/imagenesResul.java
+++ src/Dado/imagenesResul.java
@@ -18,12 +18,23 @@ public class imagenesResul {
     public ImageIcon Imaen(int vDados) {
         switch (vDados) {
             case 1:
-                icoImagen = new ImageIcon(getClass().getResource("/Imagenes/white.jpg"));
+                icoImagen = new ImageIcon(getClass().getResource("/Imagenes/1.png"));
                 break;
             case 2:
-                icoImagen = new ImageIcon(getClass().getResource("/Imagenes/black.jpg"));
+                icoImagen = new ImageIcon(getClass().getResource("/Imagenes/2.png"));
                 break;
-           
+            case 3:
+                icoImagen = new ImageIcon(getClass().getResource("/Imagenes/3.png"));
+                break;
+            case 4:
+                icoImagen = new ImageIcon(getClass().getResource("/Imagenes/4.png"));
+                break;
+            case 5:
+                icoImagen = new ImageIcon(getClass().getResource("/Imagenes/5.png"));
+                break;
+            case 6:
+                icoImagen = new ImageIcon(getClass().getResource("/Imagenes/6.png"));
+                break;
         }
         return icoImagen;
     }
